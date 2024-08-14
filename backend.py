087ab8bc8abd7632898af04b8acc149208b7bf43
zzsxd <nikitacapkov06@gmail.com>
2024-08-14 19:10:05 +0300

    fix a few bugs, delete a needless functions in backend

--- backend.py
+++ backend.py
@@ -14,9 +14,6 @@ class DbAct:
         super(DbAct, self).__init__()
         self.__db = db
 
-    def save_photo(self, byte_row, name):
-        with open(name, 'wb') as photo:
-            photo.write(byte_row)
 
     def get_user_id_from_topic(self, topic_id):
         data = self.__db.db_read("SELECT user_id FROM users WHERE topic_review_id = ?", (topic_id, ))
@@ -27,26 +24,6 @@ class DbAct:
         if not self.user_is_existed(user_id):
             self.__db.db_write('INSERT INTO users (user_id, first_name, last_name, nick_name, question_open, have_bonus) VALUES (?, ?, ?, ?, ?, ?)', (user_id, first_name, last_name, nick_name, False, False))
 
-    def update_question_status(self, user_id, status):
-        self.__db.db_write('UPDATE users SET question_open = ? WHERE user_id = ?', (status, user_id))
-
-    def get_question_status(self, topic_id):
-        data = self.__db.db_read('SELECT question_open FROM users WHERE topic_question_id = ?', (topic_id,))
-        if len(data) > 0:
-            if data[0][0] == 1:
-                ans = True
-            else:
-                ans = False
-            return ans
-
-    def get_question_status_user_id(self, user_id):
-        data = self.__db.db_read('SELECT question_open FROM users WHERE user_id = ?', (user_id,))
-        if len(data) > 0:
-            if data[0][0] == 1:
-                ans = True
-            else:
-                ans = False
-            return ans
 
     def user_is_existed(self, user_id):
         data = self.__db.db_read('SELECT count(*) FROM users WHERE user_id = ?', (user_id, ))
@@ -57,43 +34,5 @@ class DbAct:
                 status = False
             return status
 
-    def bonus_already_get(self, user_id):
-        data = self.__db.db_read('SELECT have_bonus FROM users WHERE user_id = ?', (user_id, ))
-        print(data)
-        if len(data) > 0:
-            if data[0][0] == 1:
-                status = True
-            else:
-                status = False
-            return status
-
-    def update_quest_id(self, user_id, topic_question_id):
-        self.__db.db_write('UPDATE users SET topic_question_id = ? WHERE user_id = ?', (topic_question_id, user_id))
-
     def update_review_id(self, user_id, topic_review_id):
         self.__db.db_write('UPDATE users SET topic_review_id = ? WHERE user_id = ?', (topic_review_id, user_id))
-
-    def get_quest_id(self, user_id):
-        data = self.__db.db_read('SELECT topic_question_id FROM users WHERE user_id = ?', (user_id, ))
-        if len(data) > 0:
-            return data[0][0]
-
-    def get_review_id(self, user_id):
-        data = self.__db.db_read('SELECT topic_review_id FROM users WHERE user_id = ?', (user_id, ))
-        if len(data) > 0:
-            return data[0][0]
-
-    def get_phone_numer_from_topic(self, topic_id):
-        data = self.__db.db_read('SELECT phone_number FROM users WHERE topic_review_id = ?', (topic_id, ))
-        if len(data) > 0:
-            return data[0][0]
-
-    def get_question_id(self, topic_id):
-        data = self.__db.db_read('SELECT user_id FROM users WHERE topic_question_id = ?', (topic_id, ))
-        if len(data) > 0:
-            return data[0][0]
-
-    def user_id_from_question_id(self, user_id):
-        data = self.__db.db_read('SELECT topic_question_id FROM users WHERE user_id = ?', (user_id, ))
-        if len(data) > 0:
-            return data[0][0]
